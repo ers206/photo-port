e7ed3d1b7052070617dcee184a3f144ee06a0269
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-10-03 10:57:06 -0500

    starting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom'; //instead of import ReactDOM from ‘react-dom’ ?
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = document.getElementById('root');
+render(<App />, root);
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// render(<App />, root);
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
